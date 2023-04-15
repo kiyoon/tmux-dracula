fa352d294112f6df806c4bae0a890c8d1943be31
Kiyoon Kim <yoonkr33@gmail.com>
2023-04-15 16:31:57 +0100

    fix: gpu

--- scripts/gpu_usage.sh
+++ scripts/gpu_usage.sh
@@ -6,26 +6,25 @@ current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $current_dir/utils.sh
 
 get_gpu() {
-  if command -v nvidia-smi > /dev/null 2>&1; then
-    usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ sum += $0 } END { printf("%d%%\n", sum / NR) }')
-  else
-    usage='-'
-  fi
-  normalize_percent_len $usage
+	if command -v nvidia-smi > /dev/null 2>&1; then
+		usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ sum += $0 } END { printf("%d\n", sum / NR) }')
+		if [ "$usage" -ge 0 ] || [ "$usage" -le 100 ]; then
+			normalize_percent_len "${usage}%"
+		else
+			echo "-"
+		fi
+	else
+		echo "-"
+	fi
 }
 
 main() {
-  # storing the refresh rate in the variable RATE, default is 5
-  RATE=$(get_tmux_option "@dracula-refresh-rate" 5)
-  gpu_label=$(get_tmux_option "@dracula-gpu-usage-label" "GPU")
-  gpu_usage=$(get_gpu)
-
-  if [ "$gpu_usage" -lt 0 ] || [ "$gpu_usage" -gt 100 ]; then
-    echo "$gpu_label  ERR"
-  else
-    echo "$gpu_label  $gpu_usage"
-  fi
-  sleep $RATE
+	# storing the refresh rate in the variable RATE, default is 5
+	RATE=$(get_tmux_option "@dracula-refresh-rate" 5)
+	gpu_label=$(get_tmux_option "@dracula-gpu-usage-label" "GPU")
+	gpu_usage=$(get_gpu)
+	echo "$gpu_label  $gpu_usage"
+	sleep $RATE
 }
 
 # run the main driver
